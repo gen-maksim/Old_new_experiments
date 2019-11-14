7b213e64897ca271d9b963eb0a4ca03f472d8545
Maxim <gen-maksim@mail.ru>
2019-11-14 21:57:55 +0300

    made more

--- app/Bowling.php
+++ app/Bowling.php
@@ -8,32 +8,89 @@ class Bowling extends Model
 {
     protected $score;
     protected $strikes = [];
+    protected $frame = [[]];
     protected $last_shots;
 
-    public function addShot($shot, $attempt_num) {
+//    public function addShot($shot, $attempt_num) {
+//        $come_for_strike = false;
+//        for ($key = 0; $key < count($this->strikes); $key++) {
+//            $come_for_strike = true;
+//            $this->strikes[$key]['count']--;
+//            $this->strikes[$key]['subtotal'] += $shot;
+//            dump($attempt_num);
+//            dump($this->strikes);
+//            if ($this->strikes[$key]['count'] == 0) {
+//                $this->score += $this->strikes[$key]['subtotal'];
+////                array_splice($this->strikes, $key);
+//            }
+//        }
+//        dump($this->score);
+//        if ($shot === 10) {
+//            $this->pushToStrike($attempt_num);
+//        } elseif (!$come_for_strike) {
+//            $this->score += $shot;
+//        }
+////        if (count($this->last_shots['strike']) == 3) {
+////            $this->score = array_sum($this->last_shots['strike']);
+////            $this->last_shots['strike'] = [];
+////        }
+//    }
+    public function addShot($shot) {
+        $this->pushToFrame($shot);
+        if ($shot == 10) {
+            $this->pushToStrike();
+        }
+        $this->checkStrike($shot);
+    }
+
+    public function checkStrike($shot)
+    {
         for ($key = 0; $key < count($this->strikes); $key++) {
             $this->strikes[$key]['count']--;
             $this->strikes[$key]['subtotal'] += $shot;
-            dump($this->strikes);
             if ($this->strikes[$key]['count'] == 0) {
                 $this->score += $this->strikes[$key]['subtotal'];
-                array_splice($this->strikes, $key);
             }
         }
-        if ($shot === 10) {
-            $this->pushToStrike($attempt_num);
-        } else {
-            $this->score += $shot;
-        }
-//        if (count($this->last_shots['strike']) == 3) {
-//            $this->score = array_sum($this->last_shots['strike']);
-//            $this->last_shots['strike'] = [];
-//        }
     }
 
-    public function pushToStrike($attempt_num) {
+    public function getFrames()
+    {
+        return $this->frame;
+    }
+    public function getLastFrame()
+    {
+        return $this->frame[count($this->frame) - 1];
+    }
+
+    public function CreateFrame()
+    {
+        $this->frame[] = [];
+    }
+
+    public function pushToFrame($shot)
+    {
+        if (!$this->frameIsOpened() or $shot == 10){
+            if (array_sum($this->getLastFrame()) < 10) {
+                $this->score += array_sum($this->getLastFrame());
+            }
+            $this->CreateFrame();
+        }
+        array_push($this->frame[count($this->frame) - 1], $shot);
+    }
+
+    public function frameIsOpened()
+    {
+        return (count($this->getLastFrame()) < 2 and array_sum($this->getLastFrame()) < 10);
+    }
+
+    public function getStrikes()
+    {
+        return $this->strikes;
+    }
+
+    public function pushToStrike() {
         $this->strikes[] = [
-            'attempt_num' => $attempt_num,
             'count' => 2,
             'subtotal' => 10,
         ];
--- tests/Unit/BowlingTest.php
+++ tests/Unit/BowlingTest.php
@@ -8,15 +8,6 @@ use App\Bowling;
 
 class BowlingTest extends TestCase
 {
-    /*
-     * 10+ 12 22
-     * 10+2 12
-     * +2 2
-     * 36
-     * frame
-     * shot
-     *
-     * */
     /**@test
      * A basic test example.
      *
@@ -26,20 +17,83 @@ class BowlingTest extends TestCase
     {
         $bowling = new Bowling();
 
-        $bowling->addShot(10, 1);
-        $bowling->addShot(10, 2);
-        $bowling->addShot(10, 3);
-        $bowling->addShot(1, 4);
-//        $bowling->addShot(1, 5);
+        $bowling->addShot(10);
+        $bowling->addShot(10);
+        $bowling->addShot(10);
+        $bowling->addShot(1);
+        $bowling->addShot(3);
 //        $bowling->addScore(2);
 
-        $this->assertEquals(65, $bowling->getScore());
+        $this->assertEquals(69, $bowling->getScore());
     }
 
-//    public function test_that_it_can_count_attempts()
-//    {
-//        $bowling = new Bowling();
-//
-//        $bowling->addAttempt(1);
-//    }
+    public function test_that_it_can_push_to_frame()
+    {
+        $bowling = new Bowling();
+
+        $bowling->pushToFrame(5);
+        $bowling->pushToFrame(3);
+        $bowling->pushToFrame(10);
+        $bowling->pushToFrame(8);
+
+        $this->assertEquals([[5,3],[10],[8]], $bowling->getFrames());
+    }
+
+    public function test_that_it_can_check_frame()
+    {
+        $bowling = new Bowling();
+
+        $bowling->pushToFrame(10);
+        $bowling->pushToFrame(10);
+
+
+        $this->assertEquals(false, $bowling->frameIsOpened());
+    }
+
+    public function test_that_it_can_push_score_and_create_frame()
+    {
+        $bowling = new Bowling();
+
+        $bowling->pushToFrame(10);
+        $bowling->pushToFrame(5);
+        $bowling->pushToFrame(3);
+        $bowling->pushToFrame(3);
+        
+
+        $this->assertEquals(true, $bowling->frameIsOpened());
+        $this->assertEquals(8, $bowling->getScore());
+    }
+
+    public function test_that_it_can_push_to_strike()
+    {
+        $bowling = new Bowling();
+        
+        $bowling->pushToStrike();
+        
+        $this->assertEquals([[
+            'count' => 2,
+            'subtotal' => 10,
+        ]], $bowling->getStrikes());
+    }
+
+    public function test_that_it_can_count_strikes()
+    {
+        $bowling = new Bowling();
+
+        $bowling->pushToStrike();
+        $bowling->checkStrike(5);
+
+        $this->assertEquals([[
+            'count' => 1,
+            'subtotal' => 15,
+        ]], $bowling->getStrikes());
+
+        $bowling->checkStrike(3);
+
+        $this->assertEquals(18, $bowling->getScore());
+        $this->assertEquals([[
+            'count' => 0,
+            'subtotal' => 18,
+        ]], $bowling->getStrikes());
+    }
 }
