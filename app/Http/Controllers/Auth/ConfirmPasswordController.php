f784436810c40ece947b00691490c89c8668a196
Maxim <gen-maksim@mail.ru>
2019-12-10 08:37:44 +0300

    set redirects

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -25,7 +25,7 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/trainings';
 
     /**
      * Create a new controller instance.
